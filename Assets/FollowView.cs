2b7472fe29ac5f53756f884e5557fce6d24d3e03
Fang <951498876@qq.com>
2019-08-09 00:12:31 +0800

    Asset

---
+++ Assets/FollowView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform hero;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - hero.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = hero.position + offset;
+    }
+}
